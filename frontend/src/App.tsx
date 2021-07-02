4443498af5aa1696af7a214c9b4c57ed4b996ffd
Renato S. Carvalho <renatoscarvalho18@gmail.com>
2021-07-02 12:49:08 -0300

    Add header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 
+import { Header } from './components/Header';
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import '../styles/header.scss';
+
+export function Header() {
+  return(
+    <div className="main">
+      <span className="logo">RetoOverflow</span>
+      <span className="user">Login</span>
+    </div>
+  );
+}
