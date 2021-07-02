78e3cc0be060e86f6c91b2b2a460ccae7ed29a5b
Renato S. Carvalho <renatoscarvalho18@gmail.com>
2021-07-02 12:13:55 -0300

    Add dynamic Threads listing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,37 +1,46 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+
+import api from '../services/api';
+
 import '../styles/home.scss';
 
-const threads = [{
-  name: "Renato",
-  date: "01/03/2021",
-  title: "Question 1?",
-  desc: "Description 1",
-  answers: 30
-}, {
-  name: "Leo",
-  date: "02/03/2021",
-  title: "Question 2?",
-  desc: "Description 2",
-  answers: 21
-}, {
-  name: "Deco",
-  date: "03/03/2021",
-  title: "Question 3?",
-  desc: "Description 3",
-  answers: 10
-}]
+interface IThread {
+  id: number;
+  author: string;
+  created_at: Date;
+  title: string;
+  description: string;
+  answers: number;
+}
 
 export function Home() {
+  const [threads, setThreads] = useState<IThread[]>([]);
+
+  useEffect(() => {
+    loadThreads();
+  }, [])
+
+  async function loadThreads() {
+    const response = await api.get("/threads");
+    setThreads(response.data);
+  }
+
+  function formatDate(date: Date) {
+    return moment(date).format("DD/MM/YYYY");
+  }
+
   return (
     <div className="main_container">
       {threads.map(item => (
         <div className="thread_container">
           <div className="thread_header">
-            <span className="user">{item.name}</span>
-            <span className="date">{item.date}</span>
+            <span className="user">{item.author}</span>
+            <span className="date">{formatDate(item.created_at)}</span>
           </div>
           <div className="thread_body">
             <span className="title">{item.title}</span>
-            <span className="desc">{item.desc}</span>
+            <span className="desc">{item.description}</span>
           </div>
           <div className="thread_footer">
             <span className="answer">{item.answers} Respostas</span>
